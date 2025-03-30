8301083f86159335f438d4e6288339852f271815
gagan052 <gagansaini7207@gmail.com>
2025-03-30 12:03:45 +0530

    backend is completed and frontend is 70% done

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,46 @@
+import User from "../models/user.model.js";
+import createError from "../utils/createError.js";
+
+export const deleteUser = async (req, res, next) => {
+  try {
+    if (!req.params.id) {
+      return next(createError(400, "User ID is required"));
+    }
+
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return next(createError(404, "User not found"));
+    }
+
+    if (req.userId !== user._id.toString()) {
+      return next(createError(403, "You can delete only your account!"));
+    }
+
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).send("deleted.");
+  } catch (err) {
+    if (err.name === "CastError") {
+      return next(createError(400, "Invalid user ID format"));
+    }
+    next(err);
+  }
+};
+export const getUser = async (req, res, next) => {
+  try {
+    if (!req.params.id) {
+      return next(createError(400, "User ID is required"));
+    }
+
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return next(createError(404, "User not found"));
+    }
+
+    res.status(200).send(user);
+  } catch (err) {
+    if (err.name === "CastError") {
+      return next(createError(400, "Invalid user ID format"));
+    }
+    next(err);
+  }
+};
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,49 +1,43 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  username:{
-    type:String,
-    required:true,
-    unique:true,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
   },
-  email:{
-    type:String,
-    required:true,
-    unique:true,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
   },
-  password:{
-    type:String,
-    required:true,
+  password: {
+    type: String,
+    required: true,
   },
-  img:{
-    type:String,
-    required:false,
+  img: {
+    type: String,
+    required: false,
   },
-  country:{
-    type:String,
-    required:true,
+  country: {
+    type: String,
+    required: true,
   },
-  phone:{
-    type:String,
-    required:false,
+  phone: {
+    type: String,
+    required: false,
   },
-  password:{
-    type:String,
-    required:true,
+  desc: {
+    type: String,
+    required: false,
   },
-  desc:{
-    type:String,
-    required:false,
+  isSeller: {
+    type: Boolean,
+    default:false
   },
-  isSeller:{
-    type:Boolean,
-    default:false,
-  },
-
-},
-{
-    timestamps:true
+},{
+  timestamps:true
 });
 
 export default mongoose.model("User", userSchema)
