d6cd58c302cf1ac4575b0591dcf2854b94b998b6
gagan052 <147720611+gagan052@users.noreply.github.com>
2025-05-30 19:56:47 +0530

    Update newRequest.js

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -5,4 +5,17 @@ const newRequest = axios.create({
   withCredentials: true,
 });
 
+newRequest.interceptors.request.use(
+  (config) => {
+    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    if (currentUser && currentUser.accessToken) {
+      config.headers.Authorization = `Bearer ${currentUser.accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default newRequest;
