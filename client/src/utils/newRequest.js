865c2bfc3d8d30b731320d09b7d50f0d57684bfd
gagan052 <147720611+gagan052@users.noreply.github.com>
2025-05-30 20:05:30 +0530

    Update newRequest.js

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://skillable-saini.onrender.com",
+  baseURL: "https://skillable-saini.onrender.com/api/",
   withCredentials: true,
 });
 
